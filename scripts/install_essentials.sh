4bcf39bc124417364a2d65dd8e08a24c1c7ce3d9
xieleo <xieleo2001@gmail.com>
2025-02-17 15:11:30 -0800

    update install

--- scripts/install_essentials.sh
+++ scripts/install_essentials.sh
@@ -3,4 +3,4 @@ DEBIAN_FRONTEND=noninteractive
 apt update && \
     apt install -y git curl software-properties-common && \
     add-apt-repository ppa:deadsnakes/ppa && \
-    apt install -y ffmpeg libsm6 libxext6 wget cmake build-essential vim tmux wget rsync unzip git-lfs 
+    apt install -y ffmpeg libsm6 libxext6 wget zsh cmake build-essential vim tmux wget rsync unzip git-lfs 
